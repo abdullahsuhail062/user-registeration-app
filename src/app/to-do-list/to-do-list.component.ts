39a9e6bd2eaf2d2c14857449bbb4df729e09b7fb
Your Name <you@example.com>
2025-03-08 20:21:46 +0500

    update

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -139,7 +139,7 @@ isTaskExist: boolean= false
       this.task = task      
       const taskCompleleted =true
       const taskTitle = title      
-      this.apiService.taskCompeletion(taskCompleleted,taskTitle).subscribe({next:(data)=>{this.task =data.success; console.log(data);
+      this.apiService.taskCompeletion(taskCompleleted,taskTitle).subscribe({next:(data)=>{this.task =data;
       
       
       },error:(error)=>{this.handleError(error)}})
