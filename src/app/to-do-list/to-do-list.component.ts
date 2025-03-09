a29bfd45886cd7d8044c72f4a97fd4d6407de9ce
Your Name <you@example.com>
2025-03-09 20:22:41 +0500

    update

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -35,7 +35,7 @@ taskTitleInput: string = ''
 taskDescriptionInput: string = ''
 isDeactive: boolean = true
 isActive: boolean = false
-items: { title: string; description: string, isEditing: boolean,completed: boolean }[] =[]
+items: { title: string; description: string, isEditing: boolean,task: boolean }[] =[]
 dialogRef: any
 convertObj: any
 listItem:any
@@ -100,7 +100,7 @@ isTaskExist: boolean= false
       
       
     
-      this.items.push({title: item.title,description: item.description, isEditing: false, completed:false});this.isTaskExistStatus(); 
+      this.items.push({title: item.title,description: item.description, isEditing: false, task:false});this.isTaskExistStatus(); 
       //this.items.unshift(item)
       localStorage.setItem('taskId',item.id);
       console.log(item.id);
